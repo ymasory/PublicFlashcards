9f037720b06d98222d9bb73f89b5f0523e4880f9
Yuvi Masory <ymasory@gmail.com>
2010-10-03 14:33:23 -0400

    print dirs

--- update-pdfs.sh
+++ update-pdfs.sh
@@ -16,7 +16,7 @@ for DIR in *; do
     fi
     cd $DIR;
     for FILE in *.flashup; do
-        echo CONVERTING $FILE
+        echo converting `pwd`/$FILE
         java -jar $HOME/workspace/Flashup/target/scala_*/flashup-*.min.jar $FILE;
         java -jar $HOME/workspace/Flashup/target/scala_*/flashup-*.min.jar -f $FILE;
         java -jar $HOME/workspace/Flashup/target/scala_*/flashup-*.min.jar -b $FILE;
