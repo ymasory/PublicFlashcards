57fe5f46c3b95ccfe5f94d463edf14bfc22acb86
Yuvi Masory <ymasory@gmail.com>
2011-01-08 21:13:37 -0500

    link to source

--- generate-html.scala
+++ generate-html.scala
@@ -1,7 +1,10 @@
 import java.io._
 import scala.io._
 
-val metaFilename = ".flashcardset"
+val MetaFilename = ".flashcardset"
+val UrlPrefix = "http://github.com/ymasory/PublicFlashcards/raw/master/"
+val FlashupExt = "flashup"
+val Sep = "/"
 
 val pwd = new File(System.getProperty("user.dir"))
 val subdirs = pwd.listFiles filter (_.isDirectory)
@@ -18,23 +21,26 @@ out println """
 </tr>
 """
 
-val metaPred = {(file: File) => new File(file, metaFilename).exists}
+val metaPred = {(file: File) => new File(file, MetaFilename).exists}
 val (metas, nots) = subdirs partition metaPred
 println("no meta file found for: ")
 nots foreach println
 
 for {dir <- metas
      deck <- dir.listFiles
-     if deck.getName.endsWith(".flashup")} {
-  val lines = Source.fromFile(new File(dir, metaFilename)).getLines.toList
+     name = deck.getName
+     if name.endsWith("." + FlashupExt)} {
+  val lines = Source.fromFile(new File(dir, MetaFilename)).getLines.toList
   val title = lines(0)
   val url = if (lines.length > 1) lines(1) else None
+  val basename = name.split("\\.").head
 
   out println "<tr>"
   out println ("<td>" + title + "</td>")
   out println ("<td>link</td>")
   out println ("<td>link</td>")
   out println ("<td>link</td>")
+  out println ("<td><a href=\"" + UrlPrefix + dir.getName + Sep + basename + "." + FlashupExt + "\">link</a></td>")
   out println "</tr>"
 }
 
